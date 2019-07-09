188a77577fd16015a7df088d6c388ecf66b33a7c
Mingna Li <mingnali@Mingnas-MacBook-Pro-2.local>
2019-07-09 14:18:34 -0400

    cleaned code

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
+// request express
 const express = require('express')
 const app = express()
+// request mustache
 let mustacheExpress = require('mustache-express');
+// request path
 var path = require('path');
+// request body parser
 var bodyParser = require('body-parser')
+// request postgresql
 var { Client } = require('pg')
 
+// request hue
+var hue = require("node-hue-api"),
+  HueApi = hue.HueApi,
+  lightState = hue.lightState,
+  hueScene = hue.scene;
 
-let meowCue = [];
 let newCue = [];
 
+// access the local database hue
 var client = new Client({
   database: 'hue'
 });
 
-// const socket = require('socket.io');
+
+
+// mustache setup
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname);
+
 // support parsing of application/json type post data
 app.use(bodyParser.json());
 
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// express static to include stylesheet
 app.use(express.static(__dirname + '/public'));
 
-
-
+// connect to postgres database
 client.connect();
 
 
 
 
-
-
-var hue = require("node-hue-api"),
-  HueApi = hue.HueApi,
-  lightState = hue.lightState,
-  hueScene = hue.scene;
-
-
+// display hue results
 var displayResult = function (result) {
   console.log(JSON.stringify(result, null, 2));
 };
@@ -54,39 +60,52 @@ var displayError = function (err) {
 var displayResults = function (result) {
   console.log(JSON.stringify(result, null, 2));
 };
+
+// hue bridge username
 var host = "192.168.1.136",
   username = "MLyxXervGUmxaOCnkQ0wOelUIoXRzEavSr6RC7Lz",
-  api = new HueApi(host, username),
-  redState = lightState.create().on().rgb(255, 0, 0).brightness(100),
+  api = new HueApi(host, username)
+
+// turn light red
+var redState = lightState.create().on().rgb(255, 0, 0).brightness(100),
+  // turn light green
   greenState = lightState.create().on().rgb(0, 255, 0).brightness(100),
+  // turn light blue
   blueState = lightState.create().on().rgb(0, 0, 255).brightness(100),
+  // turn light white
   whiteState = lightState.create().on().rgb(255, 255, 255).brightness(100),
+  // turn light off
   offstate = lightState.create(),
+  // hue scene
   scene = hueScene.create(),
-  sceneId = "Gx6i3OPr0Eqbgd7";
-whiteId = "2HEHYI-uc74MrbD";
-redgreenId = "mnzEAJgtbs7m-Z6"
+  // test scene
+  whiteId = "2HEHYI-uc74MrbD",
+  // a scene with red and green light
+  redgreenId = "mnzEAJgtbs7m-Z6"
 
 
+// TODO: display html from scene info results
+// 
+
+// home page, display cuesheet
 app.get('/', function (req, res) {
-  // res.sendFile(path.join(__dirname + '/index.html'));
   client.query("SELECT * FROM cues;", function (err, res1) {
     if (err) {
       console.log(err)
     }
     let cueSheet = res1.rows;
-
-    // client.end()
+    // render cuesheet through mustache
     res.render('index', {
       cueSheet
     })
   })
 });
 
-//check all scenes
+//go to cue
 app.post('/getCues', function (req, res) {
+  let index = -1;
+  let cueID;
   let showCue = req.body.retrieve
-  console.log(showCue);
   let query = {
     text: 'SELECT message FROM cues WHERE id=($1)',
     values: [showCue],
@@ -96,40 +115,41 @@ app.post('/getCues', function (req, res) {
     if (error) {
       console.log(error)
     }
-    let wow = JSON.parse(res1.rows)
-    // console.log(wow)
-    // console.log(wow)
-    api.activateScene(wow, function (err, result) {
+    //save the selected cue number in a variable
+    let cueNum = JSON.parse(res1.rows)
+    //activate the selected scene
+    api.activateScene(cueNum, function (err, result) {
       if (err) throw err;
       displayResults(result);
-      console.log("activate success")
+      console.log("cue activate success")
     });
   })
-  api.scenes(function (err, result) {
-    if (err) throw err;
-    // console.log(json.stringify(result));
-    // console.log(result.length)
-    // for (let i = 0; i < result.length; i++) {
-    meowCue = result;
-    // dog = document.createElement("BUTTON");
-    // console.log(result)
+  // test to see all scenes
+  // api.scenes(function (err, result) {
+  //   if (err) throw err;
+  //   // displayResults(result);
+  //   // console.log(JSON.stringify(result, null, 2))
+  //   for (let i = 0; i < result.length; i++) {
+  //     let selectCue = result[i];
+  //     if (selectCue.name === showCue) {
+  //       index = i;
+  //       cueId = result[i].id
+  //     }
 
-    // let newRec = result[result.length - 1].id);
+  //   }
+  //   api.activateScene(cueId, function (err, result) {
+  //     if (err) throw err;
+  //     displayResults(result);
+  //     console.log("cue activate success")
+  //   });
 
-  });
-  // res.render('<button>hello</button>')
+  // });
   res.redirect("/")
-  console.log("triggered")
-  // client.end()
 
 })
 
-// api.scenes(function (err, result) {
-//   if (err) throw err;
-//   console.log(result);
-// });
 
-// light sequence
+// light sequence test
 app.post('/sequence', function (req, res) {
   api.activateScene(whiteId)
     .then(displayResults)
@@ -161,22 +181,15 @@ app.post('/activate', function (req, res) {
 app.post('/record', function (req, res) {
   let newLabel = req.body.label;
   //name of the scene
-  scene.withName("newScene")
-    .withLights([1, 2, 4])
-    // .withTransitionTime(50)
-    ;
-  //create new scene
-  // api.createAdvancedScene(scene)
-  //   .then(displayResults)
-  //   .done();
+  //TODO: change withName string to newLabel
+  scene.withName(newLabel)
+    .withLights([1, 2, 4]);
+  //save the scene
   api.createAdvancedScene(scene, function (err, result) {
     if (err) throw err;
-    // consolek.log((JSON.stringify(result)).id)
-    // newCue.push(result)
+    //save scene id number into a variable to save in database
     let newRec = JSON.stringify(result.id);
-    // console.log("no stringify", result.id)
-    // console.log(result.id);
-    // displayResults(result);
+    //add scene id number and label in database
     client.query('INSERT INTO cues (message,label) VALUES ($1,$2)', [newRec, newLabel], function (error, res1) {
       if (error) {
         console.log(error)
@@ -185,11 +198,11 @@ app.post('/record', function (req, res) {
       res.redirect('/')
     })
   });
-
-
 })
 
+
 ///////light4///////////
+//change light4 to red
 app.get('/red', function (req, res) {
 
   console.log("setting red");
@@ -204,10 +217,7 @@ app.get('/red', function (req, res) {
 
 })
 
-// api.setLightState(4, redState, function (err, lights) {
-//   if (err) throw err;
-//   displayResult(lights);
-
+// change light4 to green
 app.post('/green', function (req, res) {
 
   api.setLightState(4, greenState, function (err, lights) {
@@ -218,6 +228,7 @@ app.post('/green', function (req, res) {
   console.log('redirected')
 })
 
+// change light 4 to blue
 app.post('/blue', function (req, res) {
 
   api.setLightState(4, blueState, function (err, lights) {
@@ -227,6 +238,8 @@ app.post('/blue', function (req, res) {
   res.redirect('/')
   console.log('redirected')
 })
+
+// change light 4 to white
 app.post('/white', function (req, res) {
 
   api.setLightState(4, whiteState, function (err, lights) {
@@ -237,6 +250,7 @@ app.post('/white', function (req, res) {
   console.log('redirected')
 })
 
+// turn light 4 off
 app.post('/off', function (req, res) {
 
   api.setLightState(4, offstate.off())
@@ -249,6 +263,7 @@ app.post('/off', function (req, res) {
 
 ///////////light2//////////////////
 
+// change light 2 to red
 app.post('/red2', function (req, res) {
 
   api.setLightState(2, redState, function (err, lights) {
@@ -258,6 +273,8 @@ app.post('/red2', function (req, res) {
   res.redirect('/')
   console.log('redirected')
 })
+
+// change light 2 to green
 app.post('/green2', function (req, res) {
 
   api.setLightState(2, greenState, function (err, lights) {
@@ -268,6 +285,7 @@ app.post('/green2', function (req, res) {
   console.log('redirected')
 })
 
+// change light 2 to blue
 app.post('/blue2', function (req, res) {
 
   api.setLightState(2, blueState, function (err, lights) {
@@ -277,6 +295,8 @@ app.post('/blue2', function (req, res) {
   res.redirect('/')
   console.log('redirected')
 })
+
+// change light 2 to white
 app.post('/white2', function (req, res) {
 
   api.setLightState(2, whiteState, function (err, lights) {
@@ -287,6 +307,7 @@ app.post('/white2', function (req, res) {
   console.log('redirected')
 })
 
+// turn light 2 to off
 app.post('/off2', function (req, res) {
   console.log(req.body)
   api.setLightState(2, offstate.off())
@@ -299,6 +320,8 @@ app.post('/off2', function (req, res) {
 
 
 //////////light1///////////
+
+// change light 1 to red
 app.post('/red1', function (req, res) {
 
   api.setLightState(1, redState, function (err, lights) {
@@ -308,6 +331,8 @@ app.post('/red1', function (req, res) {
   res.redirect('/')
   console.log('redirected')
 })
+
+// change light 1 to green
 app.post('/green1', function (req, res) {
 
   api.setLightState(1, greenState, function (err, lights) {
@@ -318,6 +343,7 @@ app.post('/green1', function (req, res) {
   console.log('redirected')
 })
 
+// change light 1 to blue
 app.post('/blue1', function (req, res) {
 
   api.setLightState(1, blueState, function (err, lights) {
@@ -327,6 +353,8 @@ app.post('/blue1', function (req, res) {
   res.redirect('/')
   console.log('redirected')
 })
+
+// change light 1 to white
 app.post('/white1', function (req, res) {
 
   api.setLightState(1, whiteState, function (err, lights) {
@@ -337,6 +365,7 @@ app.post('/white1', function (req, res) {
   console.log('redirected')
 })
 
+// turn light 1 off
 app.post('/off1', function (req, res) {
 
   api.setLightState(1, offstate.off())
@@ -347,11 +376,7 @@ app.post('/off1', function (req, res) {
   console.log('off')
 })
 
-//display all scenes
-// api.scenes()
-//   .then(displayResults)
-//   .done();
-
+// express, listening on port 8000
 app.listen(process.env.PORT || 8000, function () {
   console.log("listening on port 8000")
 })
